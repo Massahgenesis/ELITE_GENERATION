aeb6b53b6b149ecc1f1193c77ae6d2ba88e4e2dd
3sandre <sandrinetohe5@gmail.com>
2022-06-27 12:22:00 +0000

    ajout du formmulaire non stylisé

---
+++ Business & Healthcare School _ International Programs _ Bachelor and MBA_files/11.chunk.js
@@ -0,0 +1,17 @@
+(window["webpackJsonp__wix_faq_client_v3_dev"] = window["webpackJsonp__wix_faq_client_v3_dev"] || []).push([[11],{
+
+/***/ 75:
+/*!********************************!*\
+  !*** ./locales/widget_fr.json ***!
+  \********************************/
+/*! exports provided: noResults, mobile.choose.category.label, result.aria.label, choose.category.aria.labelledby, copy.tooltip.text, navigate.tabs.aria.label, results.aria.label, faq.empty.state.description, social.icon.aria.label.twitter, general, showingResultsFor, copy.success.text, choose.category.aria.label, display.tab.search.bar.demo, clearSearch.btn.aria.label, display.tab.title.demo, search.input.aria.label, social.icon.aria.label.linkedin, search.btn.aria.label, faq.empty.state.title, deepLink.back.btn, social.icon.aria.label.facebook, default */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */
+/***/ (function(module) {
+
+module.exports = JSON.parse("{\"noResults\":\"Désolé, aucun résultat trouvé pour cette recherche. Veuillez réessayer en utilisant d'autres mots-clés.\",\"mobile.choose.category.label\":\"Choisissez une catégorie\",\"result.aria.label\":\"résultat\",\"choose.category.aria.labelledby\":\"Choisissez une catégorie\",\"copy.tooltip.text\":\"Copier le lien de la question\",\"navigate.tabs.aria.label\":\"Questions/Réponses, utilisez les flèches droite et gauche pour naviguer entre les onglets\",\"results.aria.label\":\"résultats\",\"faq.empty.state.description\":\"Ouvrez les paramètres de l'appli pour ajouter la première question et réponse à votre site.\",\"social.icon.aria.label.twitter\":\"Twitter\",\"general\":\"General\",\"showingResultsFor\":\"Résultats de recherche pour : \",\"copy.success.text\":\"Copié\",\"choose.category.aria.label\":\"Choisissez une catégorie\",\"display.tab.search.bar.demo\":\"Vous cherchez quelque chose ?\",\"clearSearch.btn.aria.label\":\"effacer la recherche\",\"display.tab.title.demo\":\"Questions fréquemment posées\",\"search.input.aria.label\":\"saisissez un texte pour filtrer les questions\",\"social.icon.aria.label.linkedin\":\"LinkedIn\",\"search.btn.aria.label\":\"rechercher\",\"faq.empty.state.title\":\"Vous n'avez pas encore de FAQ\",\"deepLink.back.btn\":\"Tout afficher\",\"social.icon.aria.label.facebook\":\"Facebook\"}");
+
+/***/ })
+
+}]);
+//# sourceMappingURL=11.chunk.js.map
